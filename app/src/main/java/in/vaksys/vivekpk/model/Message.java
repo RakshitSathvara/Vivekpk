21bec6160a98a0247f24abaf836ab70d66fa6b89
vishal901 <vishal.patel77851@gmail.com>
2016-05-27 16:32:38 +0530

    fgvbrgtrbgrfef

---
+++ app/src/main/java/in/vaksys/vivekpk/model/Message.java
@@ -0,0 +1,16 @@
+package in.vaksys.vivekpk.model;
+
+/**
+ * Created by patel on 26-05-2016.
+ */
+public class Message {
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public Message(String msg) {
+        this.msg = msg;
+    }
+}
